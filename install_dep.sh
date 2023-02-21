c7e5b4751bedfd2c9b2bdf275e5d415c3abdfe9a
vecitipacijent <stefan.stanimirovic09@gmail.com>
2023-02-21 06:51:29 +0100

    neovim

---
+++ install_dep.sh
@@ -0,0 +1,8 @@
+# Installing the package manager
+sudo pacman -S npm
+
+# Intalling servers
+sudo npm i -g pyright
+sudo npm i -g vscode-langservers-extracted
+sudo npm i -g bash-language-server
+
---
+++ nvim/lua/packer-config/init.lua
@@ -0,0 +1,14 @@
+return require'packer'.startup(function()
+	use 'wbthomason/packer.nvim'
+    use "EdenEast/nightfox.nvim"
+    use 'nvim-tree/nvim-web-devicons'
+    use 'nvim-tree/nvim-tree.lua'
+    use 'nvim-lualine/lualine.nvim'
+    use 'kyazdani42/nvim-web-devicons'
+    use 'neovim/nvim-lspconfig'
+    use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+    use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+    use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+    use 'L3MON4D3/LuaSnip' -- Snippets plugin
+    use { "catppuccin/nvim", as = "catppuccin" }
+end)
